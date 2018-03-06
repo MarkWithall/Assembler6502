df505e37c2be2b364ee522ef33f1d30fb67d3fbe
Mark Withall <github@freudiantypo.com>
2018-03-06 20:50:50 +0000

    Write a simple prg file.

--- Assembler6502.Tests/UnitTest1.cs
+++ Assembler6502.Tests/UnitTest1.cs
@@ -2,13 +2,9 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
--- Assembler6502/Program.cs
+++ Assembler6502/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
 
 namespace Assembler6502
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No output file path specified!");
+                return 2;
+            }
+
+            var filePath = args[0];
+
+            // Swap 2 bytes in memory
+            byte[] program =
+            {
+                0x3C, 0x03, // staring memory location (828)
+                0xAD, 0x80, 0x03, // LDA $0380 ; (896)
+                0xAE, 0x81, 0x03, // LDX $0381 ; (897)
+                0x8D, 0x81, 0x03, // STA $0381
+                0x8E, 0x80, 0x03, // STX $0380
+                0x60, // RTS
+            };
+
+            try
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(program);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return 2;
+            }
+
+            return 0;
         }
     }
 }
