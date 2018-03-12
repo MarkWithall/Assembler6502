b39d2a3bbf28a3fb8aa23f799ef715397fd721df
Mark Withall <github@markwithall.com>
2018-03-12 13:20:19 +0000

    Add instructions lookup table.

---
+++ Assembler6502/AddressingMode.cs
@@ -0,0 +1,20 @@
+﻿namespace Assembler6502
+{
+    public enum AddressingMode
+    {
+        Unknown,
+        Implicit,
+        Accumulator,
+        Immediate,
+        Relative,
+        Absolute,
+        AbsoluteXIndexed,
+        AbsoluteYIndexed,
+        ZeroPage,
+        ZeroPageXIndexed,
+        ZeroPageYIndexed,
+        Indirect,
+        XIndexedIndirect,
+        IndirectYIndexed
+    }
+}
