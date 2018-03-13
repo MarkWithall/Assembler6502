efb751f231a12643724bf9fca348693b1610d776
Mark Withall <github@freudiantypo.com>
2018-03-13 17:36:31 +0000

    Handle a different implicit instruction.

--- Assembler6502.Tests/InstructionTests.cs
+++ Assembler6502.Tests/InstructionTests.cs
@@ -9,6 +9,7 @@ namespace Assembler6502.Tests
     public class InstructionTests
     {
         [TestCase(BRK, Implicit, (ushort) 0x0000, new byte[] {0x00})]
+        [TestCase(RTS, Implicit, (ushort) 0x0000, new byte[] {0x60})]
         public void Bytes(OpCode code, AddressingMode mode, ushort address, byte[] expectedBytes)
         {
             var instruciton = new Instruction
