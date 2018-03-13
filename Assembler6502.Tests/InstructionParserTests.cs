0f144dc9350c2bc6bb54237a875df32fd40c8cbf
Mark Withall <github@markwithall.com>
2018-03-13 13:09:02 +0000

    Merge tests.

--- Assembler6502.Tests/InstructionParserTests.cs
+++ Assembler6502.Tests/InstructionParserTests.cs
@@ -7,52 +7,40 @@ namespace Assembler6502.Tests
     [TestFixture]
     public class InstructionParserTests
     {
-        [TestCase("brk", BRK, Implicit)]
-        [TestCase("rts", RTS, Implicit)]
-        [TestCase("asl a", ASL, Accumulator)]
-        [TestCase("lda #$42", LDA, Immediate)]
-        [TestCase("bne *$42", BNE, Relative)]
-        [TestCase("sta $4242", STA, Absolute)]
-        [TestCase("and $4242,X", AND, AbsoluteXIndexed)]
-        [TestCase("ldx $4242,Y", LDX, AbsoluteYIndexed)]
-        [TestCase("eor $42", EOR, ZeroPage)]
-        [TestCase("ror $42,X", ROR, ZeroPageXIndexed)]
-        [TestCase("ldx $42,Y", LDX, ZeroPageYIndexed)]
-        [TestCase("jmp ($4242)", JMP, Indirect)]
-        [TestCase("ora ($42,X)", ORA, XIndexedIndirect)]
-        [TestCase("cmp ($42),Y", CMP, IndirectYIndexed)]
-        [TestCase("xxx $4242", OpCode.Unknown, Absolute)]
-        [TestCase("xxx ,,,,", OpCode.Unknown, AddressingMode.Unknown)]
-        [TestCase(" cmp  ($42),Y ", CMP, IndirectYIndexed)]
-        [TestCase("\tasl\ta\t", ASL, Accumulator)]
-        public void ParseInstruction(string instructionString, OpCode expectedOpCode, AddressingMode expectedAddressingMode)
+        [TestCase("brk", BRK, Implicit, (ushort) 0x0000)]
+        [TestCase("rts", RTS, Implicit, (ushort) 0x0000)]
+        [TestCase("asl a", ASL, Accumulator, (ushort) 0x0000)]
+        [TestCase("lda #$00", LDA, Immediate, (ushort) 0x0000)]
+        [TestCase("lda #$01", LDA, Immediate, (ushort) 0x0001)]
+        [TestCase("bne *$02", BNE, Relative, (ushort) 0x0002)]
+        [TestCase("bne *$03", BNE, Relative, (ushort) 0x0003)]
+        [TestCase("sta $0104", STA, Absolute, (ushort) 0x0104)]
+        [TestCase("and $0105,X", AND, AbsoluteXIndexed, (ushort) 0x0105)]
+        [TestCase("ldx $0106,Y", LDX, AbsoluteYIndexed, (ushort) 0x0106)]
+        [TestCase("eor $07", EOR, ZeroPage, (ushort) 0x0007)]
+        [TestCase("eor $0B", EOR, ZeroPage, (ushort) 0x000B)]
+        [TestCase("ror $08,X", ROR, ZeroPageXIndexed, (ushort) 0x0008)]
+        [TestCase("ldx $09,Y", LDX, ZeroPageYIndexed, (ushort) 0x0009)]
+        [TestCase("jmp ($010A)", JMP, Indirect, (ushort) 0x010A)]
+        [TestCase("ora ($0C,X)", ORA, XIndexedIndirect, (ushort) 0x000C)]
+        [TestCase("cmp ($0D),Y", CMP, IndirectYIndexed, (ushort) 0x000D)]
+        [TestCase("xxx $4242", OpCode.Unknown, Absolute, (ushort) 0x4242)]
+        [TestCase("xxx ,,,,", OpCode.Unknown, AddressingMode.Unknown, (ushort) 0x0000)]
+        [TestCase(" cmp  ($42),Y ", CMP, IndirectYIndexed, (ushort) 0x0042)]
+        [TestCase("\tasl\ta\t", ASL, Accumulator, (ushort) 0x0000)]
+        public void ParseInstruction(
+            string instructionString,
+            OpCode expectedOpCode,
+            AddressingMode expectedAddressingMode,
+            ushort expectedAddress)
         {
             var instruction = InstructionParser.Parse(instructionString);
             Assert.Multiple(() =>
             {
                 Assert.That(instruction.Code, Is.EqualTo(expectedOpCode));
                 Assert.That(instruction.Mode, Is.EqualTo(expectedAddressingMode));
+                Assert.That(instruction.Address, Is.EqualTo(expectedAddress));
             });
-        }
-
-        [TestCase("lda #$00", (ushort) 0x0000)]
-        [TestCase("lda #$01", (ushort) 0x0001)]
-        [TestCase("bne *$02", (ushort) 0x0002)]
-        [TestCase("bne *$03", (ushort) 0x0003)]
-        [TestCase("sta $0004", (ushort) 0x0004)]
-        [TestCase("and $0005,X", (ushort) 0x0005)]
-        [TestCase("ldx $0006,Y", (ushort) 0x0006)]
-        [TestCase("eor $07", (ushort) 0x0007)]
-        [TestCase("eor $0B", (ushort) 0x000B)]
-        [TestCase("ror $08,X", (ushort) 0x0008)]
-        [TestCase("ldx $09,Y", (ushort) 0x0009)]
-        [TestCase("jmp ($000A)", (ushort) 0x000A)]
-        [TestCase("ora ($0C,X)", (ushort) 0x000C)]
-        [TestCase("cmp ($0D),Y", (ushort) 0x000D)]
-        public void ParseInstructionForAddress(string instructionString, ushort expectedAddress)
-        {
-            var instruction = InstructionParser.Parse(instructionString);
-            Assert.AreEqual(expectedAddress, instruction.Address);
         }
     }
 }
