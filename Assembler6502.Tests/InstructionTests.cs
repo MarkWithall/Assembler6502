98c476819f08b0e9f40282790fa54ad5f4d04b2a
Mark Withall <github@freudiantypo.com>
2018-03-13 17:31:59 +0000

    Add Bytes property to instruction for BRK instruction.

---
+++ Assembler6502.Tests/InstructionTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using static Assembler6502.AddressingMode;
+using static Assembler6502.OpCode;
+
+namespace Assembler6502.Tests
+{
+    [TestFixture]
+    public class InstructionTests
+    {
+        [TestCase(BRK, Implicit, (ushort) 0x0000, new byte[] {0x00})]
+        public void Bytes(OpCode code, AddressingMode mode, ushort address, byte[] expectedBytes)
+        {
+            var instruciton = new Instruction
+            {
+                Code = code,
+                Mode = mode,
+                Address = address
+            };
+            CollectionAssert.AreEqual(expectedBytes, instruciton.Bytes.ToArray());
+        }
+    }
+}
--- Assembler6502/Instruction.cs
+++ Assembler6502/Instruction.cs
@@ -1,9 +1,20 @@
-﻿namespace Assembler6502
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Assembler6502
 {
     public class Instruction
     {
         public OpCode Code { get; set; }
         public AddressingMode Mode { get; set; }
         public ushort Address { get; set; }
+
+        public IEnumerable<byte> Bytes
+        {
+            get
+            {
+                yield return 0x00;
+            }
+        }
     }
 }
