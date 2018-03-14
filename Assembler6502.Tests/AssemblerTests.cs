13ce93629644a7f46d0148ed09556d714e09952f
Mark Withall <github@freudiantypo.com>
2018-03-14 19:09:31 +0000

    Strip comments from source code before assembling.

--- Assembler6502.Tests/AssemblerTests.cs
+++ Assembler6502.Tests/AssemblerTests.cs
@@ -60,5 +60,36 @@ namespace Assembler6502.Tests
 
             Assert.AreEqual(expectedBinary, binary);
         }
+
+        [Test]
+        public void CommentsInSourceCode()
+        {
+            string[] sourceCode =
+            {
+                "; program to swap two memory locations",
+                "",
+                "LDA $0380 ; load memory address 896 into A",
+                "LDX $0381 ; load memory address 897 into X",
+                "",
+                "STA $0381 ; store A into 897",
+                "STX $0380 ; store X into 896",
+                "",
+                "RTS ; return from subroutine"
+            };
+
+            var binary = Assembler.Assemble(sourceCode, 0x033C);
+
+            byte[] expectedBinary =
+            {
+                0x3C, 0x03, // staring memory location (828)
+                0xAD, 0x80, 0x03, // LDA $0380 ; (896)
+                0xAE, 0x81, 0x03, // LDX $0381 ; (897)
+                0x8D, 0x81, 0x03, // STA $0381
+                0x8E, 0x80, 0x03, // STX $0380
+                0x60 // RTS
+            };
+
+            Assert.AreEqual(expectedBinary, binary);
+        }
     }
 }
--- Assembler6502/Assembler.cs
+++ Assembler6502/Assembler.cs
@@ -9,13 +9,18 @@ namespace Assembler6502
         {
             var bytes = new List<byte> {(byte) startingAddress, (byte) (startingAddress >> 8)};
 
-            foreach (var line in sourceCode.Where(l => l.Trim() != string.Empty))
+            foreach (var line in sourceCode.Select(StripComments).Where(l => l.Trim() != string.Empty))
             {
                 var instruction = InstructionParser.Parse(line);
                 bytes.AddRange(instruction.Bytes);
             }
 
             return bytes.ToArray();
+        }
+
+        private static string StripComments(string line)
+        {
+            return line.Split(';')[0];
         }
     }
 }
