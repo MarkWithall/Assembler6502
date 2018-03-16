28074c7170ff26b08a11a19c546af9b8ad530933
Mark Withall <github@markwithall.com>
2018-03-16 08:12:49 +0000

    Add ignored test for final result of adding label support.

--- Assembler6502.Tests/AssemblerTests.cs
+++ Assembler6502.Tests/AssemblerTests.cs
@@ -91,5 +91,37 @@ namespace Assembler6502.Tests
 
             Assert.AreEqual(expectedBinary, binary);
         }
+
+        [Test, Ignore("Wait until the funtionality has been implemented!")]
+        public void CodeWithAddressLabels()
+        {
+            string[] sourceCode =
+            {
+                "; print 'Y' if memeory address content is non-zero, otherwise 'N'",
+                "       LDA $0380  ; load memory address into A",
+                "       BEQ *LABEL ; check if it was 0",
+                "       LDA #$59   ; load 'Y' into A",
+                "       JMP END    ; got to end",
+                "LABEL: LDA #$4E   ; load 'N' into A",
+                "END:   JSR $FFD2  ; write the character to the screen",
+                "       RTS"
+            };
+
+            var binary = Assembler.Assemble(sourceCode, 0x033C);
+
+            byte[] expectedBinary =
+            {
+                0x3C, 0x03,
+                0xAD, 0x80, 0x03, // 033C
+                0xF0, 0x05, // 033F
+                0xA9, 0x59, // 0341
+                0x4C, 0x48, 0x03, // 0343
+                0xA9, 0x4E, // 0346 (LABEL)
+                0x20, 0xD2, 0xFF, // 0348 (END)
+                0x60 // 034B
+            };
+
+            Assert.AreEqual(expectedBinary, binary);
+        }
     }
 }
