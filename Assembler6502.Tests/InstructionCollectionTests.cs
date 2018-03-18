c4afce32f90969c151ce93432095140f0089f540
Mark Withall <github@freudiantypo.com>
2018-03-18 07:50:25 +0000

    Introduction instruction collection to implement label finder.

---
+++ Assembler6502.Tests/InstructionCollectionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using static Assembler6502.AddressingMode;
+using static Assembler6502.OpCode;
+
+namespace Assembler6502.Tests
+{
+    [TestFixture]
+    public class InstructionCollectionTests
+    {
+        [Test]
+        public void AbsoluteAddressOfLabelOnFirstInstruction()
+        {
+            var collection = new InstructionCollection(0x033C)
+            {
+                new Instruction {Code = LDA, Mode = Absolute, AddressString = "$0380", Label = "START"}
+            };
+            var address = collection.AbsoluteAddressFor("START");
+            Assert.That(address, Is.EqualTo(0x033C));
+        }
+    }
+}
---
+++ Assembler6502/InstructionCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Assembler6502
+{
+    public class InstructionCollection : Collection<Instruction>, LabelFinder
+    {
+        private readonly ushort _startingAddress;
+
+        public InstructionCollection(ushort startingAddress)
+        {
+            _startingAddress = startingAddress;
+        }
+
+        public ushort AbsoluteAddressFor(string label)
+        {
+            return _startingAddress;
+        }
+
+        public ushort RelativeAddressFor(string label, Instruction relativeTo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
