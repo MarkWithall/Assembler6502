ef0e74e36c1b1a66143e344d823b2cf33a0df438
Mark Withall <mark.withall@tracsis.com>
2021-02-21 11:58:39 +0000

    Remove main boilerplate.

--- Assembler6502/Program.cs
+++ Assembler6502/Program.cs
@@ -1,48 +1,37 @@
 ﻿using System;
 using System.IO;
+using Assembler6502;
 
-namespace Assembler6502
+if (args.Length != 2)
 {
-    public static class Program
+    Console.WriteLine("No input/output file paths specified!");
+    return 2;
+}
+
+var inputFilePath = args[0];
+var outputFilePath = args[1];
+ushort startingAddress = 0x033C;
+var sourceCode = File.ReadAllLines(inputFilePath);
+var (binary, errors) = Assembler.Assemble(sourceCode, startingAddress);
+if (errors != null)
+{
+    foreach (var error in errors)
+        Console.WriteLine(error);
+    return 2;
+}
+
+try
+{
+    using (var stream = new FileStream(outputFilePath, FileMode.Create))
+    using (var writer = new BinaryWriter(stream))
     {
-        public static int Main(string[] args)
-        {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("No input/output file paths specified!");
-                return 2;
-            }
-
-            var inputFilePath = args[0];
-            var outputFilePath = args[1];
-            ushort startingAddress = 0x033C;
-
-            var sourceCode = File.ReadAllLines(inputFilePath);
-
-            var (binary, errors) = Assembler.Assemble(sourceCode, startingAddress);
-
-            if (errors != null)
-            {
-                foreach (var error in errors)
-                    Console.WriteLine(error);
-                return 2;
-            }
-
-            try
-            {
-                using (var stream = new FileStream(outputFilePath, FileMode.Create))
-                using (var writer = new BinaryWriter(stream))
-                {
-                    writer.Write(binary);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                return 2;
-            }
-
-            return 0;
-        }
+        writer.Write(binary);
     }
 }
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+    return 2;
+}
+
+return 0;
